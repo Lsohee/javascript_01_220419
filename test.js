c12aba1ae2924d2491042e3adecc016443c8d098
limsohee <limsohee0521@gmail.com>
2022-04-19 15:52:05 +0900

    자바 스크립트 시도

---
+++ test.js
@@ -0,0 +1,17 @@
+// 매개 변수 a와 b 는 값이 들어갈 공간 -> 없으면 값을 넣을 수 가 없음
+// function sum(a,b){
+//   return a + b;
+//   return : 뱉어
+
+// }
+
+// console.log(sum(9,1));
+
+
+// 내가 만들고 싶은거 = 스크롤을 내린 만큼 음악이 자연스럽게 소리가 줄었다 커졌다 하는 것
+
+function scroll(a){
+  return a/3;
+}
+
+console.log(scroll(6));
